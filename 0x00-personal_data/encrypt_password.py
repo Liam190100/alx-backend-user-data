6d31b7065297142a90777c09a33769e6191a02e6
Liam190100 <jnnjuguna8@.com>
2024-04-11 22:03:35 +0000

    personal data

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Encrypting passwords.
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hashes a password.
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks is a hashed password is a string.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
